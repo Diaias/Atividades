1fdf7500fe63b42c5a23b09d2011afe4c43acbe7
Lucas Dias <20242PF.CC0004@passofundo.ifsul.local>
2025-05-15 10:17:08 -0300

    agora tem o ordenador q n deixa repetir, e arrumei a ordem do antigo

---
+++ c++/Ordenador/OrdenadorDiferente.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    const int TAM = 10;
+    int i, j, vet[TAM], min, aux;
+
+    cout << "Mostrando vetor: \n";
+
+    for(i=0; i<TAM; i++){
+
+        vet[i]=rand()%10;
+        cout<<"\n Informe o Numero:["<<i<<"]:"<<vet[i];
+
+        for(j=0; j<i; j++){
+            if(vet[i] == vet[j]){
+                cout<<"\n[ja teve igual]:";
+                i--;
+            }
+        }
+    }
+
+    //PARTE QUE IMPORTA
+
+    for(i=0; i<TAM; i++){
+        for(j=0; j<TAM; j++){
+            //pra colocar em ordem crescente vet[j] > vet[i]
+            //pra colocar em ordem decrescente vet[j] < vet[i]
+            if(vet[j] > vet[i]){
+                min = vet[j];
+                aux = vet[j];
+                vet[j]=vet[i];
+                vet[i]=min;
+            }
+        }
+    }
+
+    //DEIXA DE IMPORTAR
+
+    cout << "\n\nVetor Ordenado:\n";
+    for(i=0; i<TAM; i++){
+        cout<<"\n["<<i<<"]:"<<vet[i];
+    }
+}
